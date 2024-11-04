6ab44d14ddaa3fb0d1768334f384d5b386b5833d
davipetrylima <161502620+davipetrylima@users.noreply.github.com>
2024-11-04 14:32:04 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include<studio.h>
+
+struct Post {
+    int comments;
+    int likes;
+};
+
+
+int main(){
+    
+    struct Post insta[s];
+    
+    insta[0].comments=10;
+    insta[0].likes=20;
+    insta[0].comments=10;
+    insta[0].likes=20;
+    insta[0].comments=10;
+    insta[0].likes=20;
+    insta[0].comments=10;
+    insta[0].likes=20;
+    insta[0].comments=10;
+    insta[0].likes=20;
+    
+    for(int i=0;i<5;i++){
+        printf("sua foto teve %d comentarios. \n",instal[i].coments);
+         printf("sua foto teve %d curtidas. \n",instal[i].likes);
+         printf("=====================================\n");
+    }
+    return 0
+    
+}
